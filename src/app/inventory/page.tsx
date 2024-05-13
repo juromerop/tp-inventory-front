c15b75b6e93779698397821460d449a79d58e9a0
Juan Pablo Garzon Parra <73046738+juromerop@users.noreply.github.com>
2024-05-13 09:38:19 -0500

    working on search on the products

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -2,7 +2,7 @@
 import { strict } from "assert";
 import React from "react";
 
-import { useState } from 'react';
+import { useState } from "react";
 
 interface Product {
   name: string;
@@ -20,36 +20,44 @@ function Card({ product }: { product: Product }) {
   return (
     <>
       <div
-        className={"mb-6 p-4 rounded-lg bg-gray-600 w-80 relative flex cursor-pointer"} 
+        className={
+          "mb-6 p-4 rounded-lg bg-gray-600 w-80 relative flex items-center cursor-pointer"
+        }
         onClick={() => setIsModalOpen(true)}
       >
-        <img 
-          src={product.imageUrl} 
+        <img
+          src={product.imageUrl}
           alt="Product"
-          className="h-full w-1/3 object-cover" 
+          className="h-full w-1/3 object-cover rounded-md shadow-xl"
         />
         <div className="ml-4 text-center">
           <h2 className="text-xl font-bold">{product.name}</h2>
           <p className="text-lg pt-3">
             {product.category} - {product.subcategory}
           </p>
-          <p className="text-md pt-4 font-extralight">{product.quantity} units</p>
+          <p className="text-md pt-4 font-extralight">
+            {product.quantity} units
+          </p>
         </div>
       </div>
 
       {isModalOpen && (
         <div className="fixed inset-0 flex items-center justify-center z-10 overflow-auto">
           <div className="bg-gray-600 p-4 rounded-lg shadow-lg text-white max-w-lg w-full max-h-full overflow-auto">
-            <h2 className="text-2xl font-bold mb-4 text-center">{product.name}</h2>
-            <img 
-              src={product.imageUrl} 
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              {product.name}
+            </h2>
+            <img
+              src={product.imageUrl}
               alt="Product"
-              className="h-64 w-full object-cover mb-4" 
+              className="h-64 w-full object-cover mb-4"
             />
             <p className="text-lg">
               {product.category} - {product.subcategory}
             </p>
-            <p className="text-md pt-2 font-extralight">{product.quantity} units</p>
+            <p className="text-md pt-2 font-extralight">
+              {product.quantity} units
+            </p>
             <p className="text-md pt-2">{product.description}</p>
             <button
               className="mt-4 text-red-500"
@@ -64,42 +72,85 @@ function Card({ product }: { product: Product }) {
   );
 }
 export default function InventoryView() {
-  const [isModalOpen, setIsModalOpen] = React.useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isSearchMenuOpen, setIsSearchMenuOpen] = useState(false);
+  const [categoryFilter, setCategoryFilter] = useState("");
+  const [subcategoryFilter, setSubcategoryFilter] = useState("");
   const products: Product[] = [
     {
       name: "Mouse Logitech G502 Hero",
-      description: "Mouse gamer Logitech G502 Hero con sensor Hero 16K y 11 botones programables.",
+      description:
+        "Mouse gamer Logitech G502 Hero con sensor Hero 16K y 11 botones programables.",
       category: "Pc y perifericos",
       subcategory: "Mouse",
       quantity: 10,
       date: "2022-01-01",
-      imageUrl: "https://tp-inventory-images.s3.us-east-2.amazonaws.com/IMG_8306.jpg", 
+      imageUrl:
+        "https://tp-inventory-images.s3.us-east-2.amazonaws.com/IMG_8306.jpg",
     },
     {
-      name: "Product 2",
-      description: "Description 2",
-      category: "category2",
-      subcategory: "subcategory2",
+      name: "Gafas filtro azul",
+      description:
+        "Gafas con filtro azul para proteger tus ojos de la luz azul de las pantallas.",
+      category: "Cuidado personal",
+      subcategory: "Salud visual",
       quantity: 20,
       date: "2022-02-02",
-      imageUrl: "https://tp-inventory-images.s3.us-east-2.amazonaws.com/IMG_8306.jpg", 
+      imageUrl:
+        "https://tp-inventory-images.s3.us-east-2.amazonaws.com/IMG_8307.jpg",
     },
-    {
-        name: "Product 2",
-        description: "Description 2",
-        category: "category2",
-        subcategory: "subcategory2",
-        quantity: 20,
-        date: "2022-02-02",
-        imageUrl: "", 
-      },
   ];
+  const filteredProducts = products.filter((product) => {
+    return (
+      product.name.toLowerCase().includes(searchTerm.toLowerCase()) &&
+      (categoryFilter === "" || product.category === categoryFilter) &&
+      (subcategoryFilter === "" || product.subcategory === subcategoryFilter)
+    );
+  });
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800 text-white">
       <button className="fixed top-0 left-0 m-4 p-2 bg-gray-600 rounded text-white">
         <a href="/menu">Back</a>
       </button>
-      <h1 className="text-4xl font-bold mb-5">Inventory</h1>
+      <h1 className="text-4xl font-bold mb-5 mt-8">Inventory</h1>
+      <input
+        type="text"
+        placeholder="Search by name"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        className="mb-4 p-2 border border-gray-300 rounded bg-white text-black"
+      />
+      <button
+        className="mb-4 p-2 bg-gray-600 rounded text-white"
+        onClick={() => setIsSearchMenuOpen(!isSearchMenuOpen)}
+      >
+        {isSearchMenuOpen ? "Hide" : "Show"} Advanced Search
+      </button>
+      {isSearchMenuOpen && (
+        <div className="mb-4 space-y-2">
+          <select
+            value={categoryFilter}
+            onChange={(e) => setCategoryFilter(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded bg-white text-black"
+          >
+            <option value="">All Categories</option>
+            {/* Add your categories here */}
+            <option value="Pc y perifericos">Pc y perifericos</option>
+            <option value="Cuidado personal">Cuidado personal</option>
+          </select>
+          <select
+            value={subcategoryFilter}
+            onChange={(e) => setSubcategoryFilter(e.target.value)}
+            className="w-full p-2 border border-gray-300 rounded bg-white text-black"
+          >
+            <option value="">All Subcategories</option>
+            {/* Add your subcategories here */}
+            <option value="Mouse">Mouse</option>
+            <option value="Salud visual">Salud visual</option>
+          </select>
+        </div>
+      )}
       {products.map((product, index) => (
         <Card key={index} product={product} />
       ))}
